5eda503a918916dca26abb75ce7ce2a93d38bfc1
Andrew Morris <andrewmorris@mailbox.org>
2021-09-13 12:51:09 -0400

    prepare to export to phyloseq

--- R/dada2.R
+++ R/dada2.R
@@ -121,4 +121,13 @@ head(taxa.print)
 save.image(file='myEnvironment.RData')
 #load('myEnvironment.RData')
 
+library(phyloseq); packageVersion("phyloseq")
+library(Biostrings); packageVersion("Biostrings")
+
+samples <- read_tsv('Output/barcode_master.tsv')
+fluxes <- read_tsv('Output/fluxes.tsv')
+
+ps <- phyloseq(otu_table(seqtab.nochim, taxa_are_rows=FALSE), 
+               sample_data(samdf), 
+               tax_table(taxa))
 
