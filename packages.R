29ee7f70758bf63a2c348d0748cf5dd9ca88d6c4
Andrew Morris <amorris3@uoregon.edu>
2022-09-14 18:30:33 +0200

    update analyses and intermediate data files

--- packages.R
+++ packages.R
@@ -3,3 +3,14 @@ library(devtools)
 # Install specific commit/version from github
 install_github("amorris28/morris", dependencies = FALSE)
 install_github("kbroman/broman", dependencies = FALSE)
+install_github("FrederickHuangLin/ANCOMBC", dependencies = FALSE)
+
+# if (!requireNamespace("BiocManager", quietly = TRUE))
+#   install.packages("BiocManager")
+
+renv::install("bioc::ALDEx2")
+renv::install("bioc::phyloseq")
+renv::install("bioc::ANCOMBC")
+renv::install("bioc::dada2")
+renv::install("bioc::decontam")
+
